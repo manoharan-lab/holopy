c8a2913bbed5a869e30edc8e273c7356ef5f412e
Rebecca Perry <rperry@seas.harvard.edu>
2011-06-10 16:48:52 -0400

    Closes bug #792946. Modifed the tests to be more lenient in the values they accept for final fit status. I also modified the tests to expect specific values for the time stamps (0 since they each just do one frame).

--- holopy/tests/test_fit.py
+++ holopy/tests/test_fit.py
@@ -37,17 +37,17 @@ sys.path.append(hp_dir)
 import numpy as np
 import holopy
 import nose
-from numpy.testing import assert_, assert_equal, assert_array_almost_equal
+from numpy.testing import assert_, assert_equal, assert_array_almost_equal, assert_array_less
 import string
 from nose.plugins.attrib import attr
 
-gold_single = np.array([1.5768,0.0001,6.62e-7,5.54e-6,5.79e-6,14.2e-6,0.6398,7.119,2])
+gold_single = np.array([1.5768,0.0001,6.62e-7,5.54e-6,5.79e-6,14.2e-6,0.6398,7.119])
 gold_dimerslow = np.array([1.6026,1.6026,0.00001,0.00001,6.857e-7,6.964e-7,
-    1.6998e-5,1.739e-5,2.0927e-5,1,-29.78,-13.83,5.447e-25,16.722,1.000,0.000])
+    1.6998e-5,1.739e-5,2.0927e-5,1,-29.78,-13.83,5.447e-25,16.722])
 gold_dimerfast = np.array([1.5999,1.5999,0.00001,0.00001,6.773e-7,6.651e-7,
     1.7186e-5,1.735e-5,2.0670e-5,0.787,-30.791,-14.038,0.00,53.655,5.000,0.000])
 gold_trimerslow = np.array([1.5894,1.598,1.599,1.00e-5,1.00e-5,1.00e-5,5.00e-7,5.00e-7,
-    5.00e-7,6.00e-6,6.00e-6,7.02e-6,0.60948,40.678,12.0806,4.438,4.795,1.00,0.00])
+    5.00e-7,6.00e-6,6.00e-6,7.02e-6,0.60948,40.678,12.0806,4.438,4.795])
 gold_trimerfast = np.array([1.5898,1.598,1.599,1.00e-5,1.00e-5,1.00e-5,5.00e-7,5.00e-7,
     5.00e-7,6.00e-6,6.00e-6,7.02e-6,0.60865,41.4538,11.113,3.638,4.8098,5.00,0.00])
 
@@ -61,8 +61,9 @@ class TestFit:
         holopy.fit(input_path)
         fit_result = np.loadtxt(result_path,skiprows=2,
             usecols=[1,2,3,4,5,6,7,8,9])
-        assert_array_almost_equal(fit_result,gold_single,
-            decimal=3,err_msg='Mie fit results from the single particle are not approx. equal to the standard fit results.')   
+        assert_array_almost_equal(fit_result[0:8],gold_single,
+            decimal=3,err_msg='Mie fit results from the single particle are not approx. equal to the standard fit results.')
+        assert_array_less(fit_result[8],5)      
 
     @attr('slow')
     def test_tmatrix_noisydimer_slow(self):
@@ -73,8 +74,10 @@ class TestFit:
         holopy.fit(input_path)
         fit_result = np.loadtxt(result_path,skiprows=2,
             usecols=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
-        assert_array_almost_equal(fit_result,gold_dimerslow,
+        assert_array_almost_equal(fit_result[0:14],gold_dimerslow,
             decimal=3,err_msg='Fit results from the dimer are not approx. equal to the standard fit results.')
+        assert_array_less(fit_result[14],5)
+        assert_equal(fit_result[15],0)
 
     def test_tmatrix_noisydimer_fast(self): #this fit stops after 5 iterations to be a faster fit test
         path = os.path.abspath(holopy.__file__)
@@ -97,9 +100,11 @@ class TestFit:
         holopy.fit(input_path)
         fit_result = np.loadtxt(result_path,skiprows=2,
             usecols=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]) 
-        assert_array_almost_equal(fit_result,gold_trimerslow,
+        assert_array_almost_equal(fit_result[0:17],gold_trimerslow,
             decimal=3,err_msg='Fit results from the trimer are not approx. equal to the standard fit results.')
-    
+        assert_array_less(fit_result[17],5)
+        assert_equal(fit_result[18],0)   
+        
     def test_tmatrix_noisytrimer_fast(self):
         path = os.path.abspath(holopy.__file__)
         path = string.rstrip(path, chars='__init__.pyc')+'tests/exampledata/'
