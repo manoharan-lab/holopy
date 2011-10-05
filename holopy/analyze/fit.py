Fix overlap checking.  It snuck by because the incorrect distance computation caused it to never trigger
change sphere cluster to return ndarrays for x, y, z, ...

holopy/analyze/fit.py
@@ -174,7 +174,8 @@ def make_residual(holo, scatterer, theory, scale=1.0, fixed = [],
         except UnrealizableScatterer:
             print("Fitter asked for a value which the scattering theory " +
                   "thought was unphysical or uncomputable, returning NaN")
-            return np.nan
+            error = np.NaN*np.ones(holo.size)
+            return error
 
         return cost_func(holo, calculated).ravel()
 
@@ -225,6 +226,7 @@ def minimize(residual, algorithm='nmpfit', guess=None, lb=None , ub=None,
         from holopy.third_party import nmpfit
         def resid_wrapper(p, fjac=None):
             status = 0
+            resid = residual(p)
             return [status, residual(p)]
 
         parinfo = []

holopy/process/math.py
@@ -138,5 +138,5 @@ def rotation_matrix(theta, phi, psi):
        [-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)]]
 
 def cartesian_distance(p1, p2):
-    return sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2+(p1[2]-p2[2])**2)
+    return np.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2+(p1[2]-p2[2])**2)
 

scatterpy/scatterer/spherecluster.py
@@ -111,9 +111,11 @@ class SphereCluster(Composite):
             self.scatterers = spheres
 
     def valid(self):
-        for s1 in self.scatterers:
-            for s2 in self.scatterers:
+        for i, s1 in enumerate(self.scatterers):
+            for j in range(i+1, len(self.scatterers)):
+                s2= self.scatterers[j]
                 if cartesian_distance(s1.center, s2.center) < (s1.r + s2.r):
+                    print cartesian_distance(s1.center, s2.center)
                     return False
 
         return True
@@ -154,26 +156,27 @@ class SphereCluster(Composite):
     
     @property
     def n(self):
-        return [s.n for s in self.scatterers]
+        return np.array([s.n for s in self.scatterers])
     @property
     def n_real(self):
-        return [s.n.real for s in self.scatterers]
+        return np.array([s.n.real for s in self.scatterers])
     @property
     def n_imag(self):
-        return [s.n.imag for s in self.scatterers]
+        return np.array([s.n.imag for s in self.scatterers])
     @property
     def r(self):
-        return [s.r for s in self.scatterers]
+        return np.array([s.r for s in self.scatterers])
     @property
     def x(self):
-        return [s.x for s in self.scatterers]
+        return np.array([s.x for s in self.scatterers])
     @property
     def y(self):
-        return [s.y for s in self.scatterers]
+        return np.array([s.y for s in self.scatterers])
     @property
     def z(self):
-        return [s.z for s in self.scatterers]
+        return np.array([s.z for s in self.scatterers])
     @property
     def centers(self):
-        return [s.center for s in self.scatterers]
+        return np.array([s.center for s in self.scatterers])
 
+#def rotate(