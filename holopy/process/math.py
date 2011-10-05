09bd00a4935078dd4389c4732e6eb570ba8c8bd0
Thomas Dimiduk <tdimiduk@physics.harvard.edu>
2011-10-05 13:36:45 -0400

    rotation matrix fix.  Thanks becca for finding this

--- holopy/process/math.py
+++ holopy/process/math.py
@@ -133,10 +133,6 @@ def rotation_matrix(theta, phi, psi):
     """
     return [[cos(theta)*cos(psi), -cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi),
         sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi)],
-       [cos(theta)*sin(psi), cos(theta)*cos(psi)+sin(phi)*sin(theta)*sin(psi),
-        -sin(phi)*cos(psi)+cos(phi)*sin(theta)],
+       [cos(theta)*sin(psi), cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi),
+        -sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi)],
        [-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)]]
-
-
-
-        
