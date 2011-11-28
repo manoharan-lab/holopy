c2a517e52e7bffc7078820171ff24dd98d4493b5
Jerome Fung <fung@physics.harvard.edu>
2011-11-28 18:56:41 -0500

    forgot to add relevant file

---
+++ scatterpy/scatterer/rigidclusters.py
@@ -0,0 +1,35 @@
+# Copyright 2011, Vinothan N. Manoharan, Thomas G. Dimiduk, Rebecca
+# W. Perry, Jerome Fung, and Ryan McGorty
+#
+# This file is part of Holopy.
+#
+# Holopy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Holopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Holopy.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+Defines specific rigid sphere cluster geometries.
+
+.. moduleauthor:: Jerome Fung <fung@physics.harvard.edu>
+'''
+
+from holopy.process.math import rotation_matrix
+from spherecluster import SphereCluster
+
+class BentTrimer(SphereCluster):
+    '''
+    '''
+    def __init__(self, n = None, r = None, xc = None, yc = None, zc = None,
+                 gap_12 = 0., gap_13 = 0., theta = 180., euler_alpha = 0.,
+                 euler_beta = 0., euler_gamma = 0.):
+        pass
+
