a3a8301ae327f4194a675ea46ee364aa32773172
Rebecca Perry <rperry@seas.harvard.edu>
2012-01-25 11:31:45 -0500

    added myself to the author list

--- holopy/vis/vis3d.py
+++ holopy/vis/vis3d.py
@@ -8,7 +8,7 @@ except ImportError:
 def show_sphere_cluster(s,color):
     # I think scale factor needs to be 2 because mayavi probably interprets 4th
     # argument as a diameter, we keep track of radii
-    mlab.points3d(s.x, s.y, s.z, s.r, scale_factor=2.0, resolution=32,color=color)
+    mlab.points3d(s.x, s.y, s.z, s.r, scale_factor=2.0, resolution=32, color=color)
 
 def volume_contour(d, voxel):
     vol = mlab.pipeline.scalar_field(d)
