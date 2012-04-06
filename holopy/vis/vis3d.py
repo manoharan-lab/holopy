43a975565be70627264959e91721461c0b89fe58
Rebecca Perry <rperry@seas.harvard.edu>
2012-04-06 18:22:57 -0400

    added rainbow color-coding option to 3dvis to distinguish particles

--- holopy/vis/vis3d.py
+++ holopy/vis/vis3d.py
@@ -1,3 +1,6 @@
+from matplotlib import cm
+from numpy import arange
+
 # Mayavi moved namespaces in the upgrade to 2.4, this try black will allow using
 # either the new or old namespace.
 try:
@@ -6,9 +9,18 @@ except ImportError:
     from enthought.mayavi import mlab
 
 def show_sphere_cluster(s,color):
-    # I think scale factor needs to be 2 because mayavi probably interprets 4th
+    # scale factor is 2 because mayavi interprets 4th
     # argument as a diameter, we keep track of radii
-    mlab.points3d(s.x, s.y, s.z, s.r, scale_factor=2.0, resolution=32, color=color)
+    # view is chosen to be looking from the incoming laser's point of view
+    if color=='rainbow':
+        for i in arange(0,len(s.x)):
+            print cm.gist_rainbow(float(i)/len(s.x))[0:3]
+            numberofcolors=max(10,len(s.x))
+            mlab.points3d(s.x[i], s.y[i], s.z[i], s.r[i], scale_factor=2.0, resolution=32, color=cm.gist_rainbow(float(i)/numberofcolors)[0:3])
+            mlab.view(-90,0,s.z[:].mean())
+    else:
+        mlab.points3d(s.x, s.y, s.z, s.r, scale_factor=2.0, resolution=32, color=color)
+        mlab.view(-90,0,s.z[:].mean())
 
 def volume_contour(d, voxel):
     vol = mlab.pipeline.scalar_field(d)
