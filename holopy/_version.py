45e7a0c0e9178640da358981895416abafd8a873
Thomas Dimiduk <tdimiduk@physics.harvard.edu>
2012-06-12 12:23:25 -0400

    fix holopy._version so that it does the right thing if imported from outside a repo or if bzr is not present.

--- holopy/_version.py
+++ holopy/_version.py
@@ -1,5 +1,20 @@
 from subprocess import Popen, PIPE
+import os.path
 
-revno = Popen(['bzr','revno'],stdout=PIPE).communicate()[0].strip()
-__version__ = "1.9."+str(revno)
+major_version = "1.9"
+
+holopy_dir = os.path.split(__file__)[0]
+
+try:
+    # attempt to get a minor version number based on the bzr revision number
+    revno = Popen(['bzr','revno'],stdout=PIPE,
+                  cwd=holopy_dir).communicate()[0].strip()
+except OSError:
+    # couldn't call bzr, so just set the revno to 0
+    revno = 0
+if revno == '':
+    revno = 0
+    
+__version__ = major_version+"."+str(revno)
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
+
