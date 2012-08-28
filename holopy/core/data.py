Fix a bug in core.tests.assert_obj_close that was causing it to silently pass without actually testing in certain cases.  This exposed a few other bugs.  This commit runs a few of them down but it was getting too big so I am pushing it without fixing everything because there are some bigger architectural changes that I think need to happen.
Give data.set_metadata options to not overwrite existing metadata

Futz with handling of positions somewhat.  It is better than it was, but I think it needs a reachitecting.

Resample test was wrong, corrected.

Add some more context information to asserts on a few tests.

holopy/core/data.py
@@ -79,10 +79,14 @@ class DataTarget(HolopyObject):
         for key, item in self._metadata.iteritems():
             setattr(self, key, item)
 
-    def set_metadata(self, no_set_none = True, **kwargs):
+    def set_metadata(self, overwrite = True, overwrite_with_none = False, **kwargs):
         if not hasattr(self, '_metadata'):
             self._metadata = {}
-        if no_set_none:
+        if not overwrite:
+            for key, val in kwargs.iteritems():
+                if key not in self._metadata:
+                    self._metadata[key] = val
+        elif not overwrite_with_none:
             for key, val in kwargs.iteritems():
                 if val is None:
                     if key not in self._metadata:
@@ -212,7 +216,13 @@ class Data(DataTarget, np.ndarray):
 
     def __init__(self, arr, positions = None, dtype = None, *args, **kwargs):
         super(Data, self).__init__(positions = positions, *args, **kwargs)
+        if hasattr(self.positions, 'shape'):
+            # if positions is a Grid it should always have the same shape as
+            # us.
+            # TODO: refactor things so that this is necessarily true
+            self.positions.shape = self.shape
 
+        
     def __repr__(self):
         array_repr = repr(self.view(np.ndarray))[6:-1]
         holopy_obj_repr = super(Data, self).__repr__()

holopy/core/tests/common.py
@@ -76,35 +76,47 @@ def assert_parameters_allclose(actual, desired, rtol=1e-7, atol = 0):
         assert_obj_close(actual, desired)
     else:
         assert_allclose(actual, desired, rtol=rtol, atol = atol)
+            
+        
+def assert_obj_close(actual, desired, rtol=1e-7, atol = 0, context = 'tested_object'):
+    if isinstance(actual, np.ndarray) or isinstance(desired, np.ndarray):
+        assert_allclose(actual, desired, rtol = rtol, atol = atol, err_msg=context)
 
-def assert_obj_close(actual, desired, rtol=1e-7, atol = 0, context = None):
-    if context is None:
-        context = 'tested_object'
-    elif hasattr(actual, '_dict'):
-        assert_obj_close(actual._dict, desired._dict)
+    if isinstance(actual, dict) and isinstance(desired, dict):
+        for key, val in actual.iteritems():
+            assert_obj_close(actual[key], desired[key], context =
+                             '{0}[{1}]'.format(context, key))
+    elif hasattr(actual, '_dict') and hasattr(desired, '_dict'):
+        assert_obj_close(actual._dict, desired._dict, rtol=rtol, atol=atol,
+                         context = "{0}._dict".format(context))
     elif isinstance(actual, (list, tuple)):
-        assert_equal(len(actual), len(desired))
+        assert_equal(len(actual), len(desired), err_msg=context)
         for i, item in enumerate(actual):
             assert_obj_close(actual[i], desired[i], context =
                              '{0}[{1}]'.format(context, i))
     elif isinstance(actual, types.MethodType):
-        assert_equal(actual.im_func.func_name, desired.im_func.func_name)
+        assert_equal(actual.im_func.func_name, desired.im_func.func_name,
+                     err_msg=context)
+
+        # We want to treat Mie.calc_holo and Mie().calc_holo as equal, this code
+        # here instantiates a class if possible so these match
         act_obj = actual.im_self
+        try:
+            act_obj = act_obj()
+        except TypeError:
+            pass
         des_obj = desired.im_self
-        if act_obj is None and des_obj is not None:
-            act_obj = actual.im_class()
-        if act_obj is not None and des_obj is None:
-            des_obj = desired.im_class()
-        assert_obj_close(act_obj, des_obj)
+        try:
+            des_obj = des_obj()
+        except TypeError:
+            pass
+
+        # now actually compare things 
+        assert_obj_close(act_obj, des_obj, rtol=rtol, atol=atol,
+                         context = context)
     elif hasattr(actual, '__dict__'):
         for key, val in actual.__dict__.iteritems():
-            assert_obj_close(getattr(actual, key), getattr(desired, key), rtol,
+            assert_obj_close(getattr(actual, key), getattr(desired, key), rtol, atol,
                              context = context+'.'+key)
-    elif actual is not None and not np.isscalar(actual):
-        for i, item in enumerate(actual):
-            assert_obj_close(actual[i], desired[i], rtol, atol, context)
     else:
-        try:
-            assert_equal(actual, desired)
-        except AssertionError as e:
-            raise AssertionError("\nIn {0}{1}".format(context, str(e)))
+        assert_equal(actual, desired, err_msg=context)

holopy/core/tests/test_data.py
@@ -58,8 +58,8 @@ def test_Image():
 
 def test_resample():
     i = Image(np.arange(16).reshape((4,4)), pixel_size = 1)
-    assert_obj_close(i.resample((2, 2)), Image([[  5.,   6.],
-                                                [  9.,  10.]],
-                                               pixel_size=np.array([ 0.5,  0.5]))) 
+    assert_obj_close(i.resample((2, 2)),
+                     Image([[  5.,   6.], [  9.,  10.]],
+                           pixel_size=np.array([ 2, 2])), context = 'image') 
 
-    assert_obj_close(i, Image(np.arange(16).reshape((4,4)), pixel_size = 1))
+    assert_obj_close(i, Image(np.arange(16).reshape((4,4)), pixel_size = 1), context='image')

holopy/fitting/tests/test_fit.py
@@ -279,7 +279,7 @@ def test_minimizer():
                  Parameter(name='c', guess = 3)]
     minimizer = Nmpfit()
     result, minimization_details = minimizer.minimize(parameters, cost_func)
-    assert_obj_close(gold_dict, result)
+    assert_obj_close(gold_dict, result, context = 'basic_minimized_parameters')
 
     # test inadequate specification
     with assert_raises(ParameterSpecificationError):
@@ -305,7 +305,7 @@ def test_minimizer():
     assert_equal(parinfo[2]['limits'], np.array([0., 12.])/3.)
     assert_allclose(parinfo[2]['step'], 1e-4/3.)
     assert_equal(parinfo[2]['limited'], [True, True])
-    assert_obj_close(gold_dict, result2)
+    assert_obj_close(gold_dict, result2, context = 'minimized_parameters_with_parinfo')
     
 
 @attr('fast')
@@ -333,7 +333,7 @@ def test_serialization():
     temp.seek(0)
     loaded = load(temp)
 
-    assert_obj_close(result, loaded)
+    assert_obj_close(result, loaded, context = 'serialized_result')
 
 
 @attr('slow')

holopy/propagation/convolution_propagation.py
@@ -115,14 +115,15 @@ def propagate(data, target, gradient_filter=False):
         # check if supplied distances are in a regular grid
         dd = np.diff(d)
         if np.allclose(dd[0], dd):
+            # shape of none will have the shape inferred from arr
             positions = Grid(spacing = np.append(data.positions.spacing, dd[0]),
-                             shape = arr.shape)
+                             shape = None)
         else:
             positions = UnevenGrid(spacing = (data.positions.spacing[0],
-                                    data.positions.spacing[1], d),
-                                   shape = arr.shape)
+                                              data.positions.spacing[1], d),
+                                   shape = None)
         res =  Volume(arr, positions = positions)
-    res.set_metadata(**data._metadata)
+    res.set_metadata(overwrite = False, **data._metadata)
     return res
 
 def apply_trans_func(ft, G):

holopy/propagation/tests/test_propagation.py
@@ -28,12 +28,13 @@ class test_propagation():
     def __init__(self):
         self.optics = Optics(.66, 1.33)
         im_target = ImageTarget(pixel_size = .1, shape = 100, optics = self.optics)
-        sphere = Sphere(n = 1.59, center = (5, 5, 5))
+        sphere = Sphere(n = 1.59, r = .5, center = (5, 5, 5))
         self.holo = Mie.calc_holo(sphere, im_target)
 
+    # gold for this is just a snapshot on 2012-08-27 (v249), it could be wrong.  -tgd
     def test_propagate_volume(self):
-        vol_target = VolumeTarget(positions = Grid(shape = (50, 50, 50),
-                                                   spacing = .75),
+        vol_target = VolumeTarget(positions = Grid(shape = (50, 50, 20),
+                                                   spacing = .25),
                                   optics = self.optics, center = (5, 5, 5))
         
         vol = propagate(self.holo, target = vol_target)
@@ -45,9 +46,9 @@ class test_propagation():
         vol = VolumeTarget(positions = Grid(
                 shape = np.append(self.holo.shape, len(d)),
                 spacing = np.append(self.holo.positions.spacing, 1)),
-                           center = (5, 5, 7))
+                           center = (5, 5, 7.5))
         
 
         r1 = propagate(self.holo, d)
         r2 = propagate(self.holo, vol)
-        assert_obj_close(r1, r2)
+        assert_obj_close(r1, r2, context = 'propagated_volume')