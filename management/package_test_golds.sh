35f8b218d1e95f675eace0ebf73e1d0b47655003
Tom Dimiduk <tdimiduk@physics.harvard.edu>
2013-01-31 13:38:06 -0500

    add script to package test gold data

---
+++ management/package_test_golds.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright 2011-2013, Vinothan N. Manoharan, Thomas G. Dimiduk,
+# Rebecca W. Perry, Jerome Fung, and Ryan McGorty, Anna Wang
+#
+# This file is part of HoloPy.
+#
+# HoloPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HoloPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HoloPy.  If not, see <http://www.gnu.org/licenses/>.
+
+cd ..
+zip holopy-2.0.0_test_gold_data.zip holopy/*/tests/gold/full_data/*.yaml
+mv holopy-2.0.0_test_gold_data.zip management
+cd management
