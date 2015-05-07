88ead478cf6dbd8253988744b379c69ac729c413
Anna Wang <annawang@seas.harvard.edu>
2015-05-07 15:14:16 -0400

    added cylinder capability and extended precision tmatrix

--- holopy/scattering/theory/example_tmatrix.py
+++ holopy/scattering/theory/example_tmatrix.py
@@ -1,7 +1,7 @@
 import holopy as hp
 from holopy.scattering.scatterer import Axisymmetric, Spheroid
 from holopy.core import ImageSchema, Optics
-from holopy.scattering.theory import Tmatrix
+from holopy.scattering.theory import Tmatrix, DDA
 from holopy.core.math import rotation_matrix
 
 schema = ImageSchema(shape = 200, spacing = .1,
@@ -9,6 +9,8 @@ schema = ImageSchema(shape = 200, spacing = .1,
                                      polarization = [1,0]))
 
 ps = Spheroid(n = 1.585, r = [.4,1.5], rotation=[pi/2, 2], center = [10,10,20])
+holo = DDA.calc_holo(ps,schema)
+hp.show(holo)
 holo = Tmatrix.calc_holo(ps,schema)
 hp.show(holo)
 
