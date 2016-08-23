4a030dc3106c52ad68083b33dce5936b2e0a075d
Tom Dimiduk <tom@dimiduk.net>
2016-08-23 15:22:13 -0400

    Add copyright headers, fixes #57

--- holopy/inference/tests/test_pickle.py
+++ holopy/inference/tests/test_pickle.py
@@ -1,3 +1,21 @@
+# Copyright 2011-2016, Vinothan N. Manoharan, Thomas G. Dimiduk,
+# Rebecca W. Perry, Jerome Fung, and Ryan McGorty, Anna Wang
+#
+# This file is part of HoloPy.
+#
+# HoloPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HoloPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HoloPy.  If not, see <http://www.gnu.org/licenses/>.
+
 from holopy.core.tests.common import assert_pickle_roundtrip, get_example_data
 
 from holopy.inference.mcmc import Emcee
