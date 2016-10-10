fix hologram calculation and scattering matrix computation

holopy/core/marray.py
@@ -750,7 +750,7 @@ ImageSchema._corresponding_marray = Image
 VolumeSchema._corresponding_marray = Volume
 VectorGridSchema._corresponding_marray = VectorGrid
 
-def ImageSchema(shape, spacing, optics, normals=(0, 0, 1)):
+def ImageSchema(shape, spacing, optics=None, normals=(0, 0, 1)):
     if np.isscalar(shape):
         shape = np.repeat(shape, 2)
     if np.isscalar(spacing):

holopy/core/metadata.py
@@ -25,12 +25,13 @@ Classes for defining metadata about experimental or calculated results.
 
 import numpy as np
 import xarray as xr
+from xarray.ufuncs import sqrt, arctan2
 from warnings import warn
 import copy
 from .helpers import _ensure_pair, _ensure_array
 from .holopy_object import HoloPyObject
 
-field_component = 'field_component'
+vector = 'vector'
 
 class Optics(HoloPyObject):
     """
@@ -105,7 +106,7 @@ class Optics(HoloPyObject):
 
     @property
     def polarization_field(self):
-        return to_field_components(self.polarization)
+        return to_vector(self.polarization)
 
     @polarization.setter
     def polarization(self, val):
@@ -255,7 +256,7 @@ class Angles(PositionSpecification):
 
 def interpret_args(schema=None, index=None, wavelen=None, polarization=None, optics=None):
     if optics is None:
-        if hasattr(schema, 'optics'):
+        if hasattr(schema, 'optics') and schema.optics is not None:
             optics = schema.optics
         else:
             optics = Optics()
@@ -264,27 +265,30 @@ def interpret_args(schema=None, index=None, wavelen=None, polarization=None, opt
     res.attrs['optics'] = optics
     return res
 
-def to_field_components(c):
+def to_vector(c):
     c = np.array(c)
     if c.shape == (2,):
         c = np.append(c, 0)
 
-    return xr.DataArray(c, coords={field_component: ['x', 'y', 'z']})
+    return xr.DataArray(c, coords={vector: ['x', 'y', 'z']})
 
-def to_spherical(a, origin, wavevec=None):
+def to_spherical(a, origin, wavevec=None, include_r=True):
     xo, yo, zo = origin
     x, y, z = a.x - xo, a.y - yo, zo - a.z
-    r = np.sqrt(x**2 + y**2 + z**2)
-    theta = np.arctan2(np.sqrt(x**2 + y**2), z)
-    phi = np.arctan2(y, x)
+    theta = arctan2(np.sqrt(x**2 + y**2), z)
+    phi = arctan2(y, x)
     phi = phi + 2*np.pi * (phi < 0)
-    if wavevec is not None:
-        rname = 'kr'
-        kr = r*wavevec
+    if include_r:
+        r = sqrt(x**2 + y**2 + z**2)
+        if wavevec is not None:
+            rname = 'kr'
+            kr = r*wavevec
+        else:
+            rname = 'r'
+            kr = r
+        return xr.DataArray(a, coords={rname: kr, 'theta': theta, 'phi': phi, 'x': a.x, 'y':a.y})
     else:
-        rname = 'r'
-        kr = r
-    return xr.DataArray(a, coords={rname: kr, 'theta': theta, 'phi': phi, 'x': a.x, 'y':a.y})
+        return xr.DataArray(a, coords={'theta': theta, 'phi': phi, 'x': a.x, 'y': a.y})
 
 def flat(a):
     return a.stack(flat=['x', 'y'])
@@ -305,6 +309,9 @@ def kr_theta_phi_flat(a, origin, wavevec=None):
     pos['r'] *= wavevec
     return pos
 
+def theta_phi_flat(a, origin):
+    return flat(to_spherical(a, origin))
+
 def make_coords(shape, spacing, z=0):
     if np.isscalar(shape):
         shape = np.repeat(shape, 2)
@@ -313,4 +320,4 @@ def make_coords(shape, spacing, z=0):
     return {'x': np.arange(shape[0])*spacing[0], 'y': np.arange(shape[1])*spacing[1], 'z': 0}
 
 def make_attrs(optics, normals):
-    return {'optics': optics, 'normals': to_field_components(normals)}
+    return {'optics': optics, 'normals': to_vector(normals)}

holopy/core/tests/test_marray.py
@@ -26,6 +26,7 @@ from .. import Optics
 from .common import assert_obj_close, get_example_data
 from ..errors import UnspecifiedPosition
 from .. import Angles
+from ..metadata import r_theta_phi_flat
 
 def test_VectorGrid():
     schema = ImageSchema(shape = (100,100), spacing = .1)
@@ -52,11 +53,13 @@ def test_zeros_like():
 
 def test_positions_in_spherical():
     schema = ImageSchema(shape = (2,2), spacing = 1)
-    spherical = schema.positions.r_theta_phi((0,0,1))
-    assert_allclose(spherical, np.array([[ 1. , 0. , 0.],
+    s = r_theta_phi_flat(schema, (0,0,1))
+    spherical = np.vstack((s.r, s.theta, s.phi)).T
+    gold = np.array([[ 1. , 0. , 0.],
        [ 1.41421356,  0.78539816,  1.57079633],
        [ 1.41421356,  0.78539816,  0.        ],
-       [ 1.73205081,  0.95531662,  0.78539816]]))
+       [ 1.73205081,  0.95531662,  0.78539816]])
+    assert_allclose(spherical, gold)
 
 def test_from1d():
     schema = ImageSchema(shape = (2,2), spacing = 1)
@@ -128,7 +131,7 @@ def test_positions_theta_phi():
 
 def test_positions_shape():
     schema = ImageSchema(shape = 105, spacing = 0.041)
-    pos = schema.positions.r_theta_phi([0,0,0])
+    pos = r_theta_phi_flat(schema, [0,0,0])
     assert_equal(pos.shape[0], schema.shape[0]**2)
 
 def test_squeeze():

holopy/scattering/calculations.py
@@ -24,7 +24,7 @@ calc_intensity and calc_holo, based on subclass's calc_field
 
 from holopy.core.holopy_object import SerializableMetaclass
 from holopy.core import Optics, Schema, Image, VectorGrid, interpret_args
-from holopy.core.metadata import field_component
+from holopy.core.metadata import vector
 from holopy.core.helpers import dict_without, is_none
 from holopy.scattering import Mie, Multisphere, Sphere, Spheres
 from holopy.scattering.theory import dda
@@ -247,7 +247,7 @@ def scattered_field_to_hologram(scat, ref, detector_normal = None):
     if detector_normal is None:
         detector_normal = (0, 0, 1)
 
-    holo = (np.abs(scat+ref)**2 * (1 - detector_normal)).sum(dim=field_component)
+    holo = (np.abs(scat+ref)**2 * (1 - detector_normal)).sum(dim=vector)
 
     return holo
 

holopy/scattering/theory/mie.py
@@ -27,12 +27,14 @@ scattered field.
 '''
 
 import numpy as np
+import xarray as xr
 from ...core.helpers import _ensure_array
 from ..errors import TheoryNotCompatibleError, UnrealizableScatterer
 from ..scatterer import Sphere, Scatterers
 from .scatteringtheory import FortranTheory
 from holopy.scattering.theory.mie_f import mieangfuncs, miescatlib
 from .mie_f.multilayer_sphere_lib import scatcoeffs_multi
+from holopy.core.metadata import theta_phi_flat, from_flat
 import copy
 
 
@@ -69,7 +71,7 @@ class Mie(FortranTheory):
         self.eps1 = eps1
         self.eps2 = eps2
         # call base class constructor
-        super(Mie, self).__init__()
+        super().__init__()
 
     def _can_handle(self, scatterer):
         return isinstance(scatterer, Sphere)
@@ -78,10 +80,13 @@ class Mie(FortranTheory):
         if isinstance(scatterer, Sphere):
             scat_coeffs = self._scat_coeffs(scatterer, schema.optics)
 
-            # TODO: actually use (rather than ignore) the phi
+            pos = theta_phi_flat(schema, scatterer.center)
+            # In the mie solution teh amplitude scattering matrix is independent of phi
             scat_matrs = [mieangfuncs.asm_mie_far(scat_coeffs, theta) for
-                          theta, phi in schema.positions.theta_phi(scatterer.center)]
-            return np.array(scat_matrs)
+                          theta in pos.theta]
+            # TODO: confirm that these are correct labels for the scattering matrix components
+            sm = xr.DataArray(scat_matrs, dims=['flat', 'Epar', 'Eperp'], coords={'flat': pos.flat, 'Epar': ['S2', 'S3'], 'Eperp': ['S4', 'S1']}, attrs=dict(optics=schema.optics))
+            return from_flat(sm)
         else:
             raise TheoryNotCompatibleError(self, scatterer)
 