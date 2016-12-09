45b4fa526541b0a7a59e1a316718e805590903b7
Solomon Barkley <barkley@g.harvard.edu>
2016-12-09 15:36:59 -0500

    send a blank config if in virtualenv

--- holopy/scattering/theory/mie_f/setup.py
+++ holopy/scattering/theory/mie_f/setup.py
@@ -36,10 +36,10 @@ import sys
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.command import build_ext
+    config = Configuration('mie_f', parent_package, top_path)
     if not hasattr(sys, 'real_prefix'):
         #we are not in a virtual_env. 
         #going to compile fortran code
-        config = Configuration('mie_f', parent_package, top_path)
         config.add_extension('uts_scsmfo',
                          ['uts_scsmfo.for',
                           '../../third_party/SBESJY.F']
@@ -53,7 +53,7 @@ def configuration(parent_package='', top_path=None):
         config.add_extension('scsmfo_min',
                          ['scsmfo_min.for']
                          )
-        return config
+    return config
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
