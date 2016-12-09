f60b05a80286b7164eefaf6bc4e11b5c231f2c57
Solomon Barkley <barkley@g.harvard.edu>
2016-12-09 15:58:25 -0500

    mock out emcee

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['xarray', 'mie_f']
+MOCK_MODULES = ['xarray', 'mie_f', 'emcee']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
