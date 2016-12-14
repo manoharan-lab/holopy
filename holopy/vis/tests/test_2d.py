bef8987e4921ca11e6d44c7e0b17a7836b46cc40
Solomon Barkley <barkley@g.harvard.edu>
2016-12-14 13:17:22 -0500

    changed from try:except to check if virtual env

--- holopy/vis/tests/test_2d.py
+++ holopy/vis/tests/test_2d.py
@@ -20,6 +20,7 @@ import holopy as hp
 from holopy.core.tests.common import get_example_data
 from nose.plugins.skip import SkipTest
 import warnings
+import sys
 
 try:
     with warnings.catch_warnings():
@@ -30,12 +31,11 @@ except ImportError:
     raise SkipTest()
 
 def test_show():
-    d = get_example_data('image0001')
-    try:
-        hp.show(d)
-    except RuntimeError:
-        # this occurs on travis since there is no display
+    if hasattr(sys, 'real_prefix'):
+        #we are in a virtualenv (eg travis), and there might be no display
         raise SkipTest()    
+    d = get_example_data('image0001')
+    hp.show(d) 
     with warnings.catch_warnings():
         warnings.simplefilter('ignore'),  (DeprecationWarning, UserWarning)
         plt.savefig(tempfile.TemporaryFile(suffix='.pdf'))
