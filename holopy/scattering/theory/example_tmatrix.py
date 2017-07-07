cc303b99f68b3d9f2134904ff4082128a600d8f0
Vinothan N. Manoharan <vnm@seas.harvard.edu>
2017-07-07 12:25:14 -0400

    fixed detector size in T-matrix example

--- holopy/scattering/theory/example_tmatrix.py
+++ holopy/scattering/theory/example_tmatrix.py
@@ -14,7 +14,7 @@ sphere = Sphere(n = 1.59, r = 0.9, center=(10, 10, 20))
 medium_index = 1.33
 illum_wavelen = 0.660
 illum_polarization = (1,0)
-detector = hp.detector_grid(shape = 100, spacing = .1)
+detector = hp.detector_grid(shape = 200, spacing = .1)
 holo_sphere = calc_holo(detector, sphere, medium_index, illum_wavelen, illum_polarization, theory=Tmatrix)
 holo_spheroid = calc_holo(detector, ps, medium_index, illum_wavelen, illum_polarization)
 #print(calc_scat_matrix(detector, ps, medium_index, illum_wavelen))
