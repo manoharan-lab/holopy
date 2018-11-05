b42aff9e0c8ae68b22cb96a158883b8b48a13d32
Solomon Barkley <barkley@g.harvard.edu>
2018-11-05 16:40:06 -0500

    combine fit and minize into single method .fit

--- holopy/inference/nmpfit.py
+++ holopy/inference/nmpfit.py
@@ -22,13 +22,17 @@ of third party minimizers.
 .. moduleauthor:: Thomas G. Dimiduk <tdimiduk@physics.harvard.edu>
 """
 
-
+import time
+import warnings
 
 import numpy as np
-from ..core.holopy_object import HoloPyObject
-from holopy.fitting.errors import MinimizerConvergenceFailed
-from .third_party import nmpfit
+
+from holopy.core.holopy_object import HoloPyObject
+from holopy.core.metadata import flat, make_subset_data
+from holopy.core.math import chisq, rsq
+from holopy.inference.third_party import nmpfit
 from holopy.scattering.errors import ParameterSpecificationError
+from holopy.fitting import FitResult
 
 
 class NmpfitStrategy(HoloPyObject):
@@ -56,6 +60,8 @@ class NmpfitStrategy(HoloPyObject):
         nmpfit documentation.
     maxiter: int
         Maximum number of Levenberg-Marquardt iterations to be performed.
+    random_subset : float
+        Fit only a randomly selected fraction of the data points in data
 
     Notes
     -----
@@ -68,22 +74,46 @@ class NmpfitStrategy(HoloPyObject):
 
     """
     def __init__(self, quiet = False, ftol = 1e-10, xtol = 1e-10, gtol = 1e-10,
-                 damp = 0, maxiter = 100):
+                 damp = 0, maxiter = 100, random_subset=None):
         self.ftol = ftol
         self.xtol = xtol
         self.gtol = gtol
         self.damp = 0
         self.maxiter = maxiter
         self.quiet = quiet
+        self.random_subset = random_subset
 
     def pars_from_minimizer(self, parameters, values):
         assert len(parameters) == len(values)
         return {par.name: par.unscale(value) for par, value in zip(parameters, values)}
 
-    def minimize(self, parameters, cost_func, debug = False):
-        # marshall the paramters into a dict of the form nmpfit wants
+    def fit(self, model, data):
+        """
+        fit a model to some data
+
+        Parameters
+        ----------
+        model : :class:`~holopy.fitting.model.Model` object
+            A model describing the scattering system which leads to your data and
+            the parameters to vary to fit it to the data
+        data : xarray.DataArray
+            The data to fit
+
+        Returns
+        -------
+        result : :class:`FitResult`
+            an object containing the best fit parameters and information about the fit
+        """
+        time_start = time.time()
+
+        if self.random_subset is None:
+            data = flat(data)
+        else:
+            data = make_subset_data(data, self.random_subset)
+
+      # marshall the parameters into a dict of the form nmpfit wants
         nmp_pars = []
-        for par in parameters:
+        for par  in model._parameters:
             d = {'parname': par.name}
             if par.limit is not None:
                 d['limited'] = [par.scale(l) is not None for l in par.limit]
@@ -113,19 +143,25 @@ class NmpfitStrategy(HoloPyObject):
 
         def resid_wrapper(p, fjac=None):
             status = 0
-            return [status, cost_func(self.pars_from_minimizer(parameters, p))]
+            return [status, model.residual(self.pars_from_minimizer(model._parameters, p), data)]
 
         # now fit it
         fitresult = nmpfit.mpfit(resid_wrapper, parinfo=nmp_pars, ftol = self.ftol,
                                  xtol = self.xtol, gtol = self.gtol, damp = self.damp,
                                  maxiter = self.maxiter, quiet = self.quiet)
 
-        result_pars = self.pars_from_minimizer(parameters, fitresult.params)
+        result_pars = self.pars_from_minimizer(model._parameters, fitresult.params)
 
         if fitresult.status == 5:
-            raise MinimizerConvergenceFailed(result_pars, fitresult)
-
-        if debug == True:
-            return result_pars, fitresult, nmp_pars
+            converged = False
+            warnings.warn("Minimizer Convergence Failed, your results \
+                                may not be correct.")
         else:
-            return result_pars, fitresult
+            converged = True
+
+        fitted_scatterer = model.scatterer.from_parameters(result_pars)
+        time_stop = time.time()
+        fitted = model._calc(result_pars, data)
+        return FitResult(result_pars, fitted_scatterer, chisq(fitted, data),
+                     rsq(fitted, data), converged, time_stop - time_start,
+                     model, self, fitresult)
