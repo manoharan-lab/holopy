4dab91553130d3eea3b5666109d473c686d4853c
Brian Leahy <bdl48@cornell.edu>
2019-03-18 16:26:42 -0400

    and labeling some slow tests as slow

--- holopy/scattering/tests/test_tmatrix.py
+++ holopy/scattering/tests/test_tmatrix.py
@@ -31,6 +31,7 @@ from ..errors import DependencyMissing
 from ...core import detector_grid, update_metadata
 from ...core.tests.common import verify
 
+
 def calc_holo(schema, scatterer, medium_index=None, illum_wavelen=None,**kwargs):
     try:
         return calc_holo_external(schema, scatterer, medium_index, illum_wavelen, **kwargs)
@@ -42,22 +43,28 @@ schema = update_metadata(detector_grid(shape = 200, spacing = .1),
                      illum_wavelen = .660, medium_index = 1.33,
                                      illum_polarization = [1,0])
 
+
 @attr('slow')
 def test_sphere():
     s = Sphere(n=1.59, r=0.9, center=(2, 2, 80))
     mie_holo = calc_holo(schema, s)
     tmat_holo = calc_holo(schema, s, theory=Tmatrix)
     assert_allclose(mie_holo, tmat_holo, atol=.008)
-    
+
+
+@attr("slow")
 def test_spheroid():
     s = Spheroid(n = 1.5, r = [.4, 1.], rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 15))
     holo = calc_holo(schema, s)
     verify(holo, 'tmatrix_spheroid')
 
+
+@attr("slow")
 def test_cylinder():
     s = Cylinder(n = 1.5, d=.8, h=2, rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 15))
     holo = calc_holo(schema, s)
     verify(holo, 'tmatrix_cylinder')
+
 
 def test_vs_dda():
     s = Spheroid(n = 1.5, r = [.4, 1.], rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 50))
@@ -67,4 +74,4 @@ def test_vs_dda():
         raise SkipTest()
     tmat_holo = calc_holo(schema, s, theory=Tmatrix)
     assert_allclose(dda_holo, tmat_holo, atol=.05)
-    
+
