7506bbad99df349599c20a19cfce30b76a0f72b1
Solomon Barkley <barkley@g.harvard.edu>
2019-03-21 16:53:46 -0400

    Raise Skiptest on test_Ellipsoid_dda if no adda

--- holopy/scattering/tests/test_dda.py
+++ holopy/scattering/tests/test_dda.py
@@ -130,14 +130,17 @@ def test_DDA_coated():
 def test_Ellipsoid_dda():
     e = Ellipsoid(1.5, r = (.5, .1, .1), center = (1, -1, 10))
     schema = detector_grid(10, .1)
-    h = calc_holo(schema, e, illum_wavelen=.66, medium_index=1.33,
+    try:
+        h = calc_holo(schema, e, illum_wavelen=.66, medium_index=1.33,
             illum_polarization = (1,0), theory=DDA(use_indicators=False))
-    cmd = DDA()._adda_ellipsoid(
+        cmd = DDA()._adda_ellipsoid(
             e, medium_wavelen=.66, medium_index=1.33, temp_dir='temp_dir')
-    cmdlist = ['-eq_rad', '0.5', '-shape', 'ellipsoid', '0.2', '0.2', '-m',
+        cmdlist = ['-eq_rad', '0.5', '-shape', 'ellipsoid', '0.2', '0.2', '-m',
                '1.1278195488721805', '0.0', '-orient', '0.0', '0.0', '0.0']
-    assert_equal(cmd, cmdlist)
-    verify(h, 'ellipsoid_dda')
+        assert_equal(cmd, cmdlist)
+        verify(h, 'ellipsoid_dda')
+    except DependencyMissing:
+        raise SkipTest()
 
 def test_janus():
     schema = detector_grid(10, .1)
