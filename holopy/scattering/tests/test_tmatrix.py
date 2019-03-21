e02f9d4b1fbf7288113c32942d4352f1f7ea9b28
Brian Leahy <bdl48@cornell.edu>
2019-03-21 11:48:24 -0400

    pep8 to test_tmatrix

--- holopy/scattering/tests/test_tmatrix.py
+++ holopy/scattering/tests/test_tmatrix.py
@@ -26,52 +26,59 @@ from numpy.testing import assert_raises, assert_allclose
 import numpy as np
 from nose.plugins.attrib import attr
 from nose.plugins.skip import SkipTest
-from .. import Tmatrix, DDA, Sphere, Spheroid, Ellipsoid, Cylinder, calc_holo as calc_holo_external
-from ..errors import DependencyMissing
-from ...core import detector_grid, update_metadata
-from ...core.tests.common import verify
+from holopy.scattering import (
+    Tmatrix, DDA, Sphere, Spheroid, Ellipsoid, Cylinder, calc_holo)
+from holopy.scattering.errors import DependencyMissing
+from holopy.core import detector_grid, update_metadata
+from holopy.core.tests.common import verify
 
 
-def calc_holo(schema, scatterer, medium_index=None, illum_wavelen=None,**kwargs):
+def calc_holo_safe(
+        schema, scatterer, medium_index=None, illum_wavelen=None, **kwargs):
     try:
-        return calc_holo_external(schema, scatterer, medium_index, illum_wavelen, **kwargs)
+        holo = calc_holo(
+            schema, scatterer, medium_index, illum_wavelen, **kwargs)
+        return holo
     except DependencyMissing:
         raise SkipTest()
 
 
-schema = update_metadata(detector_grid(shape = 200, spacing = .1),
-                     illum_wavelen = .660, medium_index = 1.33,
-                                     illum_polarization = [1,0])
+SCHEMA = update_metadata(
+    detector_grid(shape=200, spacing=0.1),
+    illum_wavelen=.660, medium_index=1.33, illum_polarization=[1, 0])
 
 
 @attr('slow')
 def test_sphere():
     s = Sphere(n=1.59, r=0.9, center=(2, 2, 80))
-    mie_holo = calc_holo(schema, s)
-    tmat_holo = calc_holo(schema, s, theory=Tmatrix)
+    mie_holo = calc_holo_safe(SCHEMA, s)
+    tmat_holo = calc_holo_safe(SCHEMA, s, theory=Tmatrix)
     assert_allclose(mie_holo, tmat_holo, atol=.008)
 
 
 @attr("slow")
 def test_spheroid():
-    s = Spheroid(n = 1.5, r = [.4, 1.], rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 15))
-    holo = calc_holo(schema, s)
+    s = Spheroid(
+        n=1.5, r=[.4, 1.], rotation=(0, np.pi/2, np.pi/2), center=(5, 5, 15))
+    holo = calc_holo_safe(SCHEMA, s)
     verify(holo, 'tmatrix_spheroid')
 
 
 @attr("slow")
 def test_cylinder():
-    s = Cylinder(n = 1.5, d=.8, h=2, rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 15))
-    holo = calc_holo(schema, s)
+    s = Cylinder(
+        n=1.5, d=.8, h=2, rotation=(0, np.pi/2, np.pi/2), center=(5, 5, 15))
+    holo = calc_holo_safe(SCHEMA, s)
     verify(holo, 'tmatrix_cylinder')
 
 
 def test_vs_dda():
-    s = Spheroid(n = 1.5, r = [.4, 1.], rotation = (0, np.pi/2, np.pi/2), center = (5, 5, 50))
+    s = Spheroid(
+        n=1.5, r=[.4, 1.], rotation=(0, np.pi/2, np.pi/2), center=(5, 5, 50))
     try:
-        dda_holo = calc_holo(schema, s, theory=DDA)
+        dda_holo = calc_holo_safe(SCHEMA, s, theory=DDA)
     except DependencyMissing:
         raise SkipTest()
-    tmat_holo = calc_holo(schema, s, theory=Tmatrix)
+    tmat_holo = calc_holo_safe(SCHEMA, s, theory=Tmatrix)
     assert_allclose(dda_holo, tmat_holo, atol=.05)
 
