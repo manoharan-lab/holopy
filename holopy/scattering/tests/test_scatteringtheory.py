1a0c3c8ea2a5cc5b93bd3810e658c3e288c31c2e
Brian Leahy <bdl48@cornell.edu>
2019-05-19 18:42:41 -0400

    Some cleaning of new test_scatteringtheory

--- holopy/scattering/tests/test_scatteringtheory.py
+++ holopy/scattering/tests/test_scatteringtheory.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy as np
 import xarray as xr
 from numpy.testing import assert_allclose, assert_equal
@@ -9,13 +11,21 @@ from holopy.scattering.theory.scatteringtheory import stack_spherical
 
 
 
-@attr("fast")
-def test_sphere_coords():
-    t = detector_grid(shape = (2,2), spacing = .1)
-    p = sphere_coords(t, wavevec=2*np.pi*1.33/.66, origin=(0,0,1))
-    pos = stack_spherical(p).T
-    assert_allclose(pos, np.array([[ 12.66157039,   0.        ,   0.        ],
-       [ 12.72472076,   0.09966865,   1.57079633],
-       [ 12.72472076,   0.09966865,   0.        ],
-       [ 12.78755927,   0.1404897 ,   0.78539816]]))
+class TestSphereCoords(unittest.TestCase):
+    @attr("fast")
+    def test_sphere_coords(self):
+        detector = detector_grid(shape=(2, 2), spacing=0.1)
+        spherical = sphere_coords(
+            detector, wavevec=2*np.pi*1.33/.66, origin=(0, 0, 1))
+        pos = stack_spherical(spherical).T
+        true_pos = np.array([
+            [ 12.66157039,   0.        ,   0.        ],
+            [ 12.72472076,   0.09966865,   1.57079633],
+            [ 12.72472076,   0.09966865,   0.        ],
+            [ 12.78755927,   0.1404897 ,   0.78539816]])
+        self.assertTrue(np.allclose(pos, true_pos))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
