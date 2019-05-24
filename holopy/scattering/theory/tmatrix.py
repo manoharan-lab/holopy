b9a12b85e79ecb0ed2ebb76052d937a97052831c
ralex0 <ralex0@users.noreply.github.com>
2019-05-24 17:11:19 -0400

    Remove deprecated tmatrix code

--- holopy/scattering/theory/tmatrix.py
+++ holopy/scattering/theory/tmatrix.py
@@ -19,13 +19,9 @@
 Compute holograms using Mishchenko's T-matrix method for axisymmetric scatterers.  Currently uses
 
 .. moduleauthor:: Anna Wang <annawang@seas.harvard.edu>
+.. moduleauthor:: Ron Alexander <ralex0@users.noreply.github.com>
 """
-import subprocess
-import tempfile
-import os
-import shutil
 import copy
-import warnings
 
 import numpy as np
 
@@ -61,19 +57,6 @@ class Tmatrix(ScatteringTheory):
     Does not handle near fields.  This introduces ~5% error at 10 microns.
 
     """
-    def __init__(self, delete=True):
-        self.delete = delete
-        path, _ = os.path.split(os.path.abspath(__file__))
-        self.tmatrix_executable = os.path.join(path, 'tmatrix_f', 'S')
-        if os.name == 'nt':
-            self.tmatrix_executable += '.exe'
-        if not os.path.isfile(self.tmatrix_executable):
-            raise DependencyMissing('Tmatrix', "Tmatrix code should compile "
-            "with the rest of HoloPy. Check that you can compile fortran code "
-            "from a makefile.")
-
-        super().__init__()
-
     def _can_handle(self, scatterer):
         return isinstance(scatterer, Sphere) or isinstance(scatterer, Cylinder) \
             or isinstance(scatterer, Spheroid)
@@ -135,87 +118,6 @@ class Tmatrix(ScatteringTheory):
         scat_matr = np.array([[s11, s12], [s21, s22]]).transpose()
         return scat_matr
 
-    # TODO: remove, deprecated
-    def _raw_scat_matrs_deprecated(self, scatterer, pos, medium_wavevec, 
-                                   medium_index):
-        temp_dir = tempfile.mkdtemp()
-
-        angles = pos.T[:, 1:] * 180/np.pi
-        outf = open(os.path.join(temp_dir, 'tmatrix_tmp.inp'), 'wb')
-
-        med_wavelen = 2*np.pi/medium_wavevec
-        if isinstance(scatterer, Sphere):
-            rxy = scatterer.r
-            rz = scatterer.r
-            iscyl = False
-            scatterer = copy.copy(scatterer)
-            scatterer.rotation = (0,0,0)
-        elif isinstance(scatterer, Spheroid):
-            rxy = scatterer.r[0]
-            rz = scatterer.r[1]
-            iscyl = False
-        elif isinstance(scatterer, Cylinder):
-            rxy = scatterer.d/2
-            rz = scatterer.h/2
-            iscyl = True
-        else:
-            # cleanup and raise error
-            outf.close()
-            shutil.rmtree(temp_dir)
-            raise TheoryNotCompatibleError(self, scatterer)
-
-        # write the info into the scattering angles file in the following order:
-        outf.write((str((3/2)**iscyl*(rz*rxy**2)**(1/3.))+'\n').encode('utf-8'))
-        outf.write((str(med_wavelen)+'\n').encode('utf-8'))
-        outf.write((str(scatterer.n.real/medium_index)+'\n').encode('utf-8'))
-        outf.write((str(scatterer.n.imag/medium_index)+'\n').encode('utf-8'))
-        outf.write((str(rxy/rz)+'\n').encode('utf-8'))
-        outf.write((str(scatterer.rotation[2]*180/np.pi)+'\n').encode('utf-8'))
-        outf.write((str(scatterer.rotation[1]*180/np.pi)+'\n').encode('utf-8'))
-        outf.write((str(-1 - iscyl)+'\n').encode('utf-8'))
-        outf.write((str(angles.shape[0])+'\n').encode('utf-8'))
-
-        # Now write all the angles
-        np.savetxt(outf, angles)
-        outf.close()
-
-        self._run_tmat_deprecated(temp_dir)
-        try:
-            tmat_result = np.loadtxt(os.path.join(temp_dir, 'tmatrix_tmp.out'))
-        except (FileNotFoundError, OSError):
-            #No output file
-            raise TmatrixFailure(os.path.join(temp_dir, 'log'))
-        if len(tmat_result)==0:
-            #Output file is empty
-            raise TmatrixFailure(os.path.join(temp_dir, 'log'))
-
-        # columns in result are
-        # s11.r s11.i s12.r s12.i s21.r s21.i s22.r s22.i
-        # should be
-        # s11 s12
-        # s21 s22
-
-        # Combine the real and imaginary components from the file into complex
-        # numbers. Then scale by -ki due to Mishchenko's conventions in eq 5. of
-        # Mishchenko, Applied Optics (2000).
-        s = tmat_result[:,0::2] + 1.0j*tmat_result[:,1::2]
-        s = s*(-2j*np.pi/med_wavelen)
-        # Now arrange them into a scattering matrix, noting that Mishchenko's 
-        #basis vectors are different from B/H, so we need to account for that.
-        scat_matr = np.array([[s[:,0], s[:,1]], [-s[:,2], -s[:,3]]]).transpose()
-
-        if self.delete:
-            shutil.rmtree(temp_dir)
-
-        return scat_matr
-
-    # TODO: remove, deprecated
-    def _run_tmat_deprecated(self, temp_dir):
-        # must give full path to executable even when specifying cwd keyword.
-        # we'll run the executable from its location in the package tree
-        subprocess.check_call(self.tmatrix_executable, cwd=temp_dir)
-        # can replace the above with subprocess run in python 3.5 and higher
-        return
 
     def _raw_fields(self, pos, scatterer, medium_wavevec, medium_index,
                     illum_polarization):
