035c14f9fa98a32fd8600eaa12bda7b867f0d73f
Solomon Barkley <barkley@g.harvard.edu>
2019-07-02 12:33:09 -0400

    fix a bug where alpha was always treated as fixed for inference on 2-colour holograms

--- holopy/inference/model.py
+++ holopy/inference/model.py
@@ -57,8 +57,11 @@ class BaseModel(HoloPyObject):
         return {par.name:par for par in self._parameters}
 
     def _get_parameter(self, name, pars, schema=None):
+        interpreted_pars = _interpret_parameters(pars)
         if name in pars.keys():
             return pars[name]
+        elif name in interpreted_pars.keys():
+            return interpreted_pars[name]
         elif hasattr(self, name):
             return getattr(self, name)
         try:
