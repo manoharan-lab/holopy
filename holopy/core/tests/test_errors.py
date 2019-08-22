23a5af5c28dc1a6adc5910d92cc6c642b7efaa47
ralex0 <ralex0@users.noreply.github.com>
2019-08-22 19:42:37 -0400

    Add fast attr to tests

--- holopy/core/tests/test_errors.py
+++ holopy/core/tests/test_errors.py
@@ -1,19 +1,27 @@
 import unittest
+
+from nose.plugins.attrib import attr
+
 from holopy.core.errors import *
 
 class TestErrors(unittest.TestCase):
+    @attr("fast")
     def test_LoadError(self):
         self.assertRaises(LoadError, _raise, LoadError('', ''))
 
+    @attr("fast")
     def test_BadImage(self):
         self.assertRaises(BadImage, _raise, BadImage())
 
+    @attr("fast")
     def test_NoMetadata(self):
         self.assertRaises(NoMetadata, _raise, NoMetadata())
 
+    @attr("fast")
     def test_CoordSysError(self):
         self.assertRaises(CoordSysError, _raise, CoordSysError())
 
+    @attr("fast")
     def test_DependencyMissing(self):
         self.assertRaises(DependencyMissing, _raise, DependencyMissing('', ''))
 
