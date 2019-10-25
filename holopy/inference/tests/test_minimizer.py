4b9008ac3008ec7effb91ee557e430d7a8a0af46
Brian Leahy <bleahy@g.harvard.edu>
2019-10-25 11:51:26 -0400

    catch warning in test_minimizer

--- holopy/inference/tests/test_minimizer.py
+++ holopy/inference/tests/test_minimizer.py
@@ -20,8 +20,9 @@
 import warnings
 
 import numpy as np
-
 from numpy.testing import assert_equal, assert_raises, assert_allclose
+from nose.plugins.attrib import attr
+
 from holopy.scattering.scatterer import Sphere, Spheres
 from holopy.core import detector_grid
 from holopy.core.tests.common import assert_obj_close
@@ -30,6 +31,8 @@ from holopy.scattering.errors import ParameterSpecificationError
 from holopy.inference import NmpfitStrategy as Nmpfit
 from holopy.inference import prior, AlphaModel
 
+
+@attr('fast')
 def test_minimizer():
     x = np.arange(-10, 10, .1)
     a = 5.3
@@ -62,8 +65,19 @@ def test_minimizer():
         result, minimization_details = cf.result, cf.details
     assert_equal(minimization_details.niter, 2) # there's always an offset of 1
 
-def test_iter_limit():
-    gold_fit_dict={'0:r': 0.52480509800531849, '1:center.1': 14.003687569304704, 'alpha': 0.93045027963762217, '0:center.2': 19.93177549652841, '1:r': 0.56292664494653732, '0:center.1': 15.000340621607815, '1:center.0': 14.020984607646726, '0:center.0': 15.000222185576494, '1:center.2': 20.115613202192328}
+
+@attr('slow')
+def test_optimization_with_maxiter_of_2():
+    gold_fit_dict = {
+        '0:r': 0.52480509800531849,
+        '1:center.1': 14.003687569304704,
+        'alpha': 0.93045027963762217,
+        '0:center.2': 19.93177549652841,
+        '1:r': 0.56292664494653732,
+        '0:center.1': 15.000340621607815,
+        '1:center.0': 14.020984607646726,
+        '0:center.0': 15.000222185576494,
+        '1:center.2': 20.115613202192328}
 
     #calculate a hologram with known particle positions to do a fit against
     schema = detector_grid(shape = 100, spacing = .1)
@@ -74,12 +88,31 @@ def test_iter_limit():
     holo = calc_holo(schema, cluster, 1.33, .66, illum_polarization=(1,0))
 
     #trying to do a fast fit:
-    guess1 = Sphere(center = (prior.Uniform(5, 25, guess = 15), prior.Uniform(5, 25, 15), prior.Uniform(5, 25, 20)), r = (prior.Uniform(.4, .6, guess = .45)), n = 1.59)
-    guess2 = Sphere(center = (prior.Uniform(5, 25, guess = 14), prior.Uniform(5, 25, 14), prior.Uniform(5, 25, 20)), r = (prior.Uniform(.4, .6, guess = .45)), n = 1.59)
-    par_s = Spheres([guess1,guess2])
+    guess1 = Sphere(
+        center=(prior.Uniform(5, 25, guess=15),
+                prior.Uniform(5, 25, 15),
+                prior.Uniform(5, 25, 20)),
+        r=(prior.Uniform(.4, .6, guess=.45)),
+        n=1.59)
+    guess2 = Sphere(
+        center=(prior.Uniform(5, 25, guess=14),
+                prior.Uniform(5, 25, 14),
+                prior.Uniform(5, 25, 20)),
+        r=(prior.Uniform(.4, .6, guess = .45)),
+        n=1.59)
+    par_s = Spheres([guess1, guess2])
 
-    model = AlphaModel(par_s, medium_index=1.33, illum_wavelen=.66, illum_polarization=(1, 0), alpha = prior.Uniform(.1, 1, .6))
+    model = AlphaModel(
+        par_s, medium_index=1.33, illum_wavelen=.66, illum_polarization=(1, 0),
+        alpha=prior.Uniform(.1, 1, .6))
     warnings.simplefilter("always")
-    result = Nmpfit(maxiter=2).optimize(model, holo)
+    optimizer = Nmpfit(maxiter=2)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        result = optimizer.optimize(model, holo)
     assert_obj_close(gold_fit_dict,result.parameters,rtol=1e-5)
+
+
+# TODO: There could be a test that the optimizer raises a warning when
+# `maxiter` iterations have passed.
 
