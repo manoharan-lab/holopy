c8ee85ecbcec63bad0d6069ad17ed84cb865139c
ralex0 <ralex0@users.noreply.github.com>
2019-11-15 19:53:09 -0500

    setup.py PEP8 edits

--- setup.py
+++ setup.py
@@ -28,12 +28,17 @@ The t-matrix and mie scattering codes require working Fortran 90 and
 C compilers, as well as f2py and cython. On Ubuntu, you will need the
 "gfortran" and "python-dev" packages installed.
 '''
-
-import glob, os, setuptools, shutil, site, sys
+import glob
+import os
+import shutil
+import site
+import sys
 from os.path import join
 
 import nose
 from numpy.distutils.core import setup, Extension
+from numpy.distutils.misc_util import Configuration
+import setuptools
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
@@ -42,77 +47,92 @@ try:
 except ImportError:
     __version__ = 'unknown'
 
+HOLOPY_NOSE_PLUGIN_LOCATION = ('holopycatchwarnings = '
+                               'holopy.core.tests.common:HoloPyCatchWarnings')
+
 hp_root = os.path.dirname(os.path.realpath(sys.argv[0]))
+
 
 class PostDevelopConfig(develop):
     """Post-installation for development mode."""
     def run(self):
         develop.run(self)
-        if os.name == 'nt': _move_msvc_libs('develop')
+        if os.name == 'nt':
+            _move_msvc_libs('develop')
 
 
 class PostInstallConfig(install):
     """Post-installation for installation mode."""
     def run(self):
         install.run(self)
-        if os.name == 'nt': _move_msvc_libs('install')
+        if os.name == 'nt':
+            _move_msvc_libs('install')
 
 
-def configuration(parent_package='',top_path=''):
+def configuration(parent_package='', top_path=''):
     # this will automatically build the scattering extensions, using the
     # setup.py files located in their subdirectories
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None,parent_package,top_path)
+    config = Configuration(None, parent_package, top_path)
 
-    pkglist=setuptools.find_packages(hp_root)
+    pkglist = setuptools.find_packages(hp_root)
     for i in pkglist:
         config.add_subpackage(i)
 
-    config.add_data_files(['.',[join(hp_root,'AUTHORS')]])
-    config.add_data_files(join(hp_root,'holopy','scattering','tests','gold','full_data','*.h5'))
-    config.add_data_files(join(hp_root,'holopy','scattering','tests','gold','*.yaml'))
-    config.add_data_files(join(hp_root,'holopy','core','tests','exampledata','*.h5'))
-    config.add_data_files(join(hp_root,'holopy','core','tests','exampledata','*.jpg'))
-    config.add_data_files(join(hp_root,'holopy','propagation','tests','gold','full_data','*.h5'))
-    config.add_data_files(join(hp_root,'holopy','propagation','tests','gold','*.yaml'))
-
+    config.add_data_files(['.', [join(hp_root, 'AUTHORS')]])
+    config.add_data_files(join(hp_root, 'holopy', 'scattering', 'tests',
+                               'gold', 'full_data', '*.h5'))
+    config.add_data_files(join(hp_root, 'holopy', 'scattering', 'tests',
+                               'gold', '*.yaml'))
+    config.add_data_files(join(hp_root, 'holopy', 'core', 'tests',
+                               'exampledata', '*.h5'))
+    config.add_data_files(join(hp_root, 'holopy', 'core', 'tests',
+                               'exampledata', '*.jpg'))
+    config.add_data_files(join(hp_root, 'holopy', 'propagation', 'tests',
+                               'gold', 'full_data', '*.h5'))
+    config.add_data_files(join(hp_root, 'holopy', 'propagation', 'tests',
+                               'gold', '*.yaml'))
     return config
 
+
 def _move_msvc_libs(mode='install'):
-    """ These dlls need to be moved if the fortran is complied in an environment
-    with MSVC 2015 as the C compiler.
+    """ These dlls need to be moved if the fortran is complied in an
+    environment with MSVC 2015 as the C compiler.
     """
     package_dir = _get_holopy_install_dir(mode)
-    lib_dir = os.path.join(package_dir, '.libs')
-    sep = os.path.sep
+    lib_dir = join(package_dir, '.libs')
 
-    tmatrix_libs = glob.glob(lib_dir + sep + 'libS.*.dll')
-    mie_libs = glob.glob(lib_dir + sep + 'libscsm*.dll')
-    mie_libs += glob.glob(lib_dir + sep + 'libmieang*.dll')
-    mie_libs += glob.glob(lib_dir + sep + 'libuts*.dll')
+    tmatrix_libs = glob.glob(join(lib_dir, 'libS.*.dll'))
+    mie_libs = glob.glob(join(lib_dir, 'libscsm*.dll'))
+    mie_libs += glob.glob(join(lib_dir, 'libmieang*.dll'))
+    mie_libs += glob.glob(join(lib_dir, 'libuts*.dll'))
 
-    tmatrix_f_dir = os.path.join(package_dir, 'scattering', 'theory', 'tmatrix_f')
-    mie_f_dir = os.path.join(package_dir, 'scattering', 'theory', 'mie_f')
+    tmatrix_f_dir = join(package_dir, 'scattering', 'theory', 'tmatrix_f')
+    mie_f_dir = join(package_dir, 'scattering', 'theory', 'mie_f')
 
-    for dll in tmatrix_libs: shutil.move(dll, tmatrix_f_dir)
-    for dll in mie_libs: shutil.move(dll, mie_f_dir)
+    for dll in tmatrix_libs:
+        shutil.move(dll, tmatrix_f_dir)
+    for dll in mie_libs:
+        shutil.move(dll, mie_f_dir)
     shutil.rmtree(lib_dir)
+
 
 def _get_holopy_install_dir(mode):
     if mode == 'install':
-        sitepackages = list(site.getsitepackages())
-        dir = [path for path in site.getsitepackages() if 'site-packages' in path]
-        assert len(dir) == 1
-        dir = os.path.join(dir[0], 'holopy')
+        hp_dir = [path for path in site.getsitepackages()
+                  if 'site-packages' in path]
+        hp_dir = os.path.join(dir[0], 'holopy')
     if mode == 'develop':
-        dir = os.path.dirname(os.path.realpath(__file__))
-        dir = os.path.join(dir, 'holopy')
-    return dir
+        hp_dir = os.path.dirname(os.path.realpath(__file__))
+        hp_dir = os.path.join(dir, 'holopy')
+    return hp_dir
+
 
 if __name__ == "__main__":
-    requires=[l for l in open(os.path.join(hp_root,"requirements.txt")).readlines() if l[0] != '#']
+    with open(os.path.join(hp_root, "requirements.txt")) as requirements:
+        requires = [l for l in requirements.readlines() if l[0] != '#']
 
     tests_require = ['memory_profiler']
+
     setup(configuration=configuration,
           name='HoloPy',
           version=__version__,
@@ -124,6 +144,7 @@ if __name__ == "__main__":
           url='http://manoharan.seas.harvard.edu/holopy',
           license='GNU GPL',
           test_suite='nose.collector',
+          entry_points={'nose.plugins.0.10': HOLOPY_NOSE_PLUGIN_LOCATION},
           package=['HoloPy'],
           cmdclass={'develop': PostDevelopConfig,
                     'install': PostInstallConfig})
