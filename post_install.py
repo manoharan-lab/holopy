cdee24b6ca37b87e37c2ea1765c0a0f3a45a122f
ralex0 <ralex0@users.noreply.github.com>
2019-11-18 14:28:59 -0500

    Postinstall script in its own file

--- post_install.py
+++ post_install.py
@@ -1,34 +1,10 @@
-# Copyright 2011-2016, Vinothan N. Manoharan, Thomas G. Dimiduk,
-# Rebecca W. Perry, Jerome Fung, Ryan McGorty, Anna Wang, Solomon Barkley
-#
-# This file is part of HoloPy.
-#
-# HoloPy is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# HoloPy is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with HoloPy.  If not, see <http://www.gnu.org/licenses/>.
-'''
-post_install.py
-
-Configuration for the post install of holopy
-'''
 import glob
 import os
-from os.path import join
 import shutil
 import site
 
 from setuptools.command.develop import develop
 from setuptools.command.install import install
-
 
 class PostDevelopConfig(develop):
     """Post-installation for development mode."""
@@ -51,15 +27,17 @@ def _move_msvc_libs(mode='install'):
     environment with MSVC 2015 as the C compiler.
     """
     package_dir = _get_holopy_install_dir(mode)
-    lib_dir = join(package_dir, '.libs')
+    lib_dir = os.path.join(package_dir, '.libs')
+    sep = os.path.sep
 
-    tmatrix_libs = glob.glob(join(lib_dir, 'libS.*.dll'))
-    mie_libs = glob.glob(join(lib_dir, 'libscsm*.dll'))
-    mie_libs += glob.glob(join(lib_dir, 'libmieang*.dll'))
-    mie_libs += glob.glob(join(lib_dir, 'libuts*.dll'))
+    tmatrix_libs = glob.glob(lib_dir + sep + 'libS.*.dll')
+    mie_libs = glob.glob(lib_dir + sep + 'libscsm*.dll')
+    mie_libs += glob.glob(lib_dir + sep + 'libmieang*.dll')
+    mie_libs += glob.glob(lib_dir + sep + 'libuts*.dll')
 
-    tmatrix_f_dir = join(package_dir, 'scattering', 'theory', 'tmatrix_f')
-    mie_f_dir = join(package_dir, 'scattering', 'theory', 'mie_f')
+    tmatrix_f_dir = os.path.join(package_dir, 'scattering', 'theory',
+                                 'tmatrix_f')
+    mie_f_dir = os.path.join(package_dir, 'scattering', 'theory', 'mie_f')
 
     for dll in tmatrix_libs:
         shutil.move(dll, tmatrix_f_dir)
@@ -70,9 +48,11 @@ def _move_msvc_libs(mode='install'):
 
 def _get_holopy_install_dir(mode):
     if mode == 'install':
+        sitepackages = list(site.getsitepackages())
         hp_dir = [path for path in site.getsitepackages()
-                  if 'site-packages' in path]
-        hp_dir = os.path.join(dir[0], 'holopy')
+               if 'site-packages' in path]
+        assert len(hp_dir) == 1
+        hp_dir = os.path.join(hp_dir[0], 'holopy')
     if mode == 'develop':
         hp_dir = os.path.dirname(os.path.realpath(__file__))
         hp_dir = os.path.join(hp_dir, 'holopy')
