1439b5a85b6d84cfb6a5abf5b4f06706a0dd8c20
Brian Leahy <bleahy@g.harvard.edu>
2019-12-06 11:55:02 -0500

    capture issue #251 with failing test

--- holopy/inference/tests/test_scipyfit.py
+++ holopy/inference/tests/test_scipyfit.py
@@ -7,7 +7,8 @@ from nose.plugins.attrib import attr
 from holopy.scattering import Sphere, Mie, calc_holo
 from holopy.core.process import normalize
 from holopy.core.tests.common import get_example_data
-from holopy.inference import AlphaModel, LeastSquaresScipyStrategy
+from holopy.inference import (
+    AlphaModel, LeastSquaresScipyStrategy, NmpfitStrategy)
 from holopy.inference.prior import Uniform
 
 
@@ -56,6 +57,48 @@ class TestLeastSquaresScipyStrategy(unittest.TestCase):
             np.isclose(result.parameters['alpha'], CORRECT_ALPHA, rtol=0.1))
         self.assertEqual(model, result.model)
 
+    @attr('medium')
+    def test_fitted_parameters_similar_to_nmpfit(self):
+        holo = normalize(get_example_data('image0001'))
+        model = make_model()
+
+        np.random.seed(40)
+        fitter_scipy = LeastSquaresScipyStrategy(npixels=500)
+        result_scipy = fitter_scipy.fit(model, holo)
+        params_scipy = result_scipy.parameters
+
+        np.random.seed(40)
+        fitter_nmp = NmpfitStrategy(npixels=500)
+        result_nmp = fitter_nmp.fit(model, holo)
+        params_nmp = result_nmp.parameters
+
+        for key in params_scipy.keys():
+            self.assertAlmostEqual(
+                params_scipy[key],
+                params_nmp[key],
+                places=4)
+
+    @attr('medium')
+    def test_fitted_uncertainties_similar_to_nmpfit(self):
+        holo = normalize(get_example_data('image0001'))
+        model = make_model()
+
+        np.random.seed(40)
+        fitter_scipy = LeastSquaresScipyStrategy(npixels=500)
+        result_scipy = fitter_scipy.fit(model, holo)
+        uncertainties_scipy = pack_uncertainties_into_dict(result_scipy)
+
+        np.random.seed(40)
+        fitter_nmp = NmpfitStrategy(npixels=500)
+        result_nmp = fitter_nmp.fit(model, holo)
+        uncertainties_nmp = pack_uncertainties_into_dict(result_nmp)
+
+        for key in uncertainties_scipy.keys():
+            self.assertAlmostEqual(
+                uncertainties_scipy[key],
+                uncertainties_nmp[key],
+                places=4)
+
 
 def make_model():
     center_guess = [
@@ -71,6 +114,11 @@ def make_model():
     theory = Mie(compute_escat_radial=False)
     model = AlphaModel(scatterer, theory=theory, alpha=alpha)
     return model
+
+
+def pack_uncertainties_into_dict(fit_result):
+    intervals = fit_result.intervals
+    return {v.name: v.plus for v in intervals}
 
 
 if __name__ == '__main__':
