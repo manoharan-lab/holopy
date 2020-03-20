f5888a890d482cc79367f7b08eadd88318d00816
Matthias Gianfelice <matthias@gianfelice.de>
2020-03-20 07:15:09 +0100

    PEP8-Update

--- holopy/core/io/io.py
+++ holopy/core/io/io.py
@@ -379,39 +379,41 @@ def save_image(filename, im, scaling='auto', depth=8):
     else:
         pilimage.fromarray(im).save(filename)
 
-def save_images(filenames, ims, scaling='auto', depth = 8):
+
+def save_images(filenames, ims, scaling='auto', depth=8):
     """
     Saves a volume as separate images (think of reconstruction volumes).
 
     Parameters
     ----------
     ims : ndarray or :class:`holopy.image.Image`
-        Images to save, with separate z-coordinates from which they each will be
-        selected.
+        Images to save, with separate z-coordinates from which they each will
+        be selected.
     filename : str
-        Filename in which to save image. Has to contain a format placeholder for
-        the z-values. E.g. "testfile%f.png"
+        Filename in which to save image. Has to contain a format placeholder
+        for the z-values. E.g. "testfile%f.png"
     scaling : 'auto', None, or (Int, Int)
-        How the images should be scaled for saving. Ignored for float output. It
-        defaults to auto, use the full range of the output format. Other options
-        are None, meaning no scaling, or a pair of integers specifying the
-        values which should be set to the maximum and minimum values of the
+        How the images should be scaled for saving. Ignored for float output.
+        It defaults to auto, use the full range of the output format. Other
+        options are None, meaning no scaling, or a pair of integers specifying
+        the values which should be set to the maximum and minimum values of the
         image format.
     depth : 8, 16 or 'float'
-        What type of image to save. Options other than 8bit may not be supported
-        for many image types. You probably don't want to save 8bit images
-        without some kind of scaling.
+        What type of image to save. Options other than 8bit may not be
+        supported for many image types. You probably don't want to save 8bit
+        images without some kind of scaling.
     """
     # TODO: Here could be a check whether ims has the correct format.
     ims = display_image(ims, scaling)
     for im in ims:
-        z = im.z.values.item(0) # TODO: There must be a nicer way…
+        z = im.z.values.item(0)  # TODO: There must be a nicer way…
         _save_im(filenames % z, im, scaling, depth)
+
 
 def _save_im(filename, im, scaling='auto', depth=8):
     """
-    Internal single-image-save-method to be used in save_images. Maybe it can be
-    merged with save_image.
+    Internal single-image-save-method to be used in save_images. Maybe it can
+    be merged with save_image.
 
     Parameters
     ----------
@@ -421,25 +423,25 @@ def _save_im(filename, im, scaling='auto', depth=8):
         Filename in which to save image. If im is an image the function should
         default to the image's name field if no filename is specified
     scaling : 'auto', None, or (Int, Int)
-        How the images should be scaled for saving. Ignored for float output. It
-        defaults to auto, use the full range of the output format. Other options
-        are None, meaning no scaling, or a pair of integers specifying the
-        values which should be set to the maximum and minimum values of the
+        How the images should be scaled for saving. Ignored for float output.
+        It defaults to auto, use the full range of the output format. Other
+        options are None, meaning no scaling, or a pair of integers specifying
+        the values which should be set to the maximum and minimum values of the
         image format.
     depth : 8, 16 or 'float'
-        What type of image to save. Options other than 8bit may not be supported
-        for many image types. You probably don't want to save 8bit images
-        without some kind of scaling.
+        What type of image to save. Options other than 8bit may not be
+        supported for many image types. You probably don't want to save 8bit
+        images without some kind of scaling.
     """
     # if we don't have an extension, default to tif
     if os.path.splitext(filename)[1] is '':
         filename += '.tif'
 
-    metadat=False
+    metadat = False
     if os.path.splitext(filename)[1] in tiflist:
         if im.name is None:
-            im.name=os.path.splitext(os.path.split(filename)[-1])[0]
-        metadat = pack_attrs(im, do_spacing = True)
+            im.name = os.path.splitext(os.path.split(filename)[-1])[0]
+        metadat = pack_attrs(im, do_spacing=True)
         # import ifd2 - hidden here since it doesn't play nice in some cases.
         from PIL.TiffImagePlugin import ImageFileDirectory_v2 as ifd2
         tiffinfo = ifd2()
