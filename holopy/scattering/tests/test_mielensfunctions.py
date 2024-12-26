91ca07fef80c5d2bb59791dde4ff56e06179bb88
Vinothan N. Manoharan <vnm@seas.harvard.edu>
2024-12-26 12:00:52 -0500

    replace np.trapz with scipy.integrate.trapezoid to avoid deprecation

--- holopy/scattering/tests/test_mielensfunctions.py
+++ holopy/scattering/tests/test_mielensfunctions.py
@@ -5,6 +5,7 @@ import itertools
 import numpy as np
 from numpy.polynomial.chebyshev import Chebyshev
 from scipy.special import jn_zeros
+from scipy.integrate import trapezoid
 
 import pytest
 
@@ -802,7 +803,7 @@ class CheckEnergyIsConserved(object):
         i2 = self.mielenscalculator._eval_mielens_i_n(krho, n=2)
 
         integrand = (np.abs(i0)**2 + np.abs(i2)**2) * krho
-        return 0.5 * np.trapz(integrand, krho)
+        return 0.5 * trapezoid(integrand, krho)
 
     def check_if_energy_is_conserved(self):
         ratio = self.get_ratio_of_powerin_to_powerout()
